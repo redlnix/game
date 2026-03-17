fe1089af9671ca4c077e49ef49263ef238972e6d
red <hopsread@gmail.com>
2026-03-16 20:46:29 -0400

    reconsidering life decisions after having to make barriers

--- script.js
+++ script.js
@@ -7,17 +7,24 @@ const player = {
   speed: 1
 }
 
-const block = {
-  x: 500,
-  y: 250,
-}
-
 const player1 = document.getElementById("player")
 
 const keys = {}
 
-const block1 = document.getElementById("block")
+const rect = player1.getBoundingClientRect()
 
+//barriers (checking for collisions)
+function checkCollision(e1, e2) { 
+  const a = e1.getBoundingClientRect()
+  const b = e2.getBoundingClientRect()
+
+  return!(
+    a.right < b.left ||
+    a.left > b.right ||
+    a.bottom < b.top ||
+    a.top > b.bottom 
+  )
+}
 
 //movement system
 function updateplayer() { 
@@ -26,15 +33,31 @@ function updateplayer() {
 }  
 document.addEventListener("keydown", (e) => keys[e.key] = true) 
 document.addEventListener("keyup", (e) => keys[e.key] = false)
+//
+
+
+//GAMELOOP, IMPORTANT
+
 function gameloop() {
+//saving old coordinates to teleport back to if collision with wall is met
+  const oldX = player.x
+  const oldY = player.y
+//
+
+//movement system in the gameloop 
   if (keys["w"]) player.y -= player.speed
   if (keys["s"]) player.y += player.speed
   if (keys["a"]) player.x -= player.speed
   if (keys["d"]) player.x += player.speed
 //  console.log(keys) only turn on for debugging!
+//  
+//updating player
   updateplayer()
+//  
+
+
+
   requestAnimationFrame(gameloop)
 }
-//movement system
 
 gameloop()
